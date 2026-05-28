1842947c25171c94172ef1bd08f9481cdcb55d2d
Vasantha <your-email@gmail.com>
2026-05-28 17:26:36 +0530

    fix errors

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'http://localhost:5174',
-        'https://personal-portfolio-iota-swart-99.vercel.app',
+        'https://personal-portfolio-iota-swart-99.vercel.app/',
     ],
     methods: ['GET', 'POST'],
  })); // your React dev URL
